6378d9517844af2b13e280ac61bb2c716350303c
meerbek.akimzhanov <meerbek.kg@gmail.com>
2021-05-08 13:48:24 +0600

    Valiadation was added

--- src/main/java/TargetHeartRatesController.java
+++ src/main/java/TargetHeartRatesController.java
@@ -19,15 +19,28 @@ public class TargetHeartRatesController {
     private Label thtrResult;
 
     @FXML
+    private  Label justText;
+
+    @FXML
     void onCalculateButtonPressed(ActionEvent event) {
-        float age = Float.parseFloat(ageTextField.getText());;
+        try {
+            float age = Float.parseFloat(ageTextField.getText());;
 
-        float HRmax = 220 - age;
-        int THTmax = (int) (HRmax*0.85);
-        int THTmin = (int) (HRmax*0.5);
+            float HRmax = 220 - age;
+            int THTmax = (int) (HRmax*0.85);
+            int THTmin = (int) (HRmax*0.5);
 
-        mhtResult.setText(HRmax+" bpm");
-        thtrResult.setText(THTmin + " - " + THTmax +" bpm");
+            mhtResult.setText(HRmax+" bpm");
+            thtrResult.setText(THTmin + " - " + THTmax +" bpm");
+        }
+        catch(Exception e) {
+            if (e.getMessage() == "empty String"){
+                justText.setText("Your age can not be empty");
+            }
+            else {
+                justText.setText("Your age must be in numbers");
+            }
+        }
 
     }
 
